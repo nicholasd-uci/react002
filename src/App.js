89d3a03563683ca585d456daa861298a08e1a14f
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-14 11:30:56 -0700

    updated a create react app without teacher

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// removed a line here
+// removed a line here
+
+function App() {
+  return (
+    // Removed the entire section that was auto populated here //
+  );
+}
+
+export default App;
